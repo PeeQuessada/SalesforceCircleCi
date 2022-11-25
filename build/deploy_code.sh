a663578499ad329031ddcfd7c3455a1da840c85f
PedroQuessadaGAF <pedro.quessada@gaf.energy>
2022-11-25 20:37:05 -0300

    test

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -1,3 +1,4 @@
-echo "Deploying code to org"
+sfdx config:set defaultusername=$SANDBOX_USERNAME
+
 sfdx sgd:source:delta --to HEAD --from HEAD^ --output .
 sfdx force:source:deploy -x package/package.xml -l RunLocalTests
