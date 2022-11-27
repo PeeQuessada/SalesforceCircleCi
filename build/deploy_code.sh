dcf6dee1dedba910d1c136f9561f407581f6197c
Pedro Quessada <pedroquessada@pop-os.localdomain>
2022-11-26 21:04:44 -0300

    test delta

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -1,5 +1,4 @@
 sfdx config:set defaultusername=DevHub
 
-mkdir changed-sources
-sfdx sgd:source:delta --to "HEAD" --from "HEAD^" --output changed-sources/ --generate-delta --source force-app/ 
+sfdx sgd:source:delta --to HEAD --from HEAD^ --output .
 sfdx force:source:deploy -x package/package.xml -l RunLocalTests
